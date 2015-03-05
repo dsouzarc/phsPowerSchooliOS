e4ef3f0877aa6c251c1cc984098460014d43d5e6
Ryan D'souza <dsouzarc@gmail.com>
2015-03-05 07:33:08 -0500

    First commit

---
+++ PHSPowerSchool/LoginToPowerSchoolViewController.h
@@ -0,0 +1,13 @@
+//
+//  LoginToPowerSchoolViewController.h
+//  PHSPowerSchool
+//
+//  Created by Ryan D'souza on 3/5/15.
+//  Copyright (c) 2015 Ryan D'souza. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LoginToPowerSchoolViewController : UIViewController
+
+@end
---
+++ PHSPowerSchool/LoginToPowerSchoolViewController.m
@@ -0,0 +1,37 @@
+//
+//  LoginToPowerSchoolViewController.m
+//  PHSPowerSchool
+//
+//  Created by Ryan D'souza on 3/5/15.
+//  Copyright (c) 2015 Ryan D'souza. All rights reserved.
+//
+
+#import "LoginToPowerSchoolViewController.h"
+
+@interface LoginToPowerSchoolViewController ()
+
+@end
+
+@implementation LoginToPowerSchoolViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
