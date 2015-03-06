fda91b2494a5f1982efcf414f121f1dd2895d186
Ryan D'souza <dsouzarc@gmail.com>
2015-03-06 07:31:13 -0500

    Initial Commit

---
+++ PHSPowerSchool/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PHSPowerSchool
+//
+//  Created by Ryan D'souza on 3/6/15.
+//  Copyright (c) 2015 Ryan D'souza. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ PHSPowerSchool/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PHSPowerSchool
+//
+//  Created by Ryan D'souza on 3/6/15.
+//  Copyright (c) 2015 Ryan D'souza. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
