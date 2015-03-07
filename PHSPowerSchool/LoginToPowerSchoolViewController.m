d82fd5ab4b4a49b210f2f5e44f7c2ffc0f9ad03a
Ryan D'souza <dsouzarc@gmail.com>
2015-03-06 23:41:15 -0500

    Parsing individual grades

--- PHSPowerSchool/LoginToPowerSchoolViewController.m
+++ PHSPowerSchool/LoginToPowerSchoolViewController.m
@@ -9,10 +9,11 @@
 #import "LoginToPowerSchoolViewController.h"
 #import "PQFBarsInCircle.h"
 #import "UICKeyChainStore.h"
+#import "TFHpple.h"
+
 #import "StudentCIass.h"
 #import "CiassInformation.h"
 #import "SettingsViewController.h"
-#import "TFHpple.h"
 
 @interface LoginToPowerSchoolViewController ()
 
@@ -51,46 +52,39 @@
     NSString *html = [[NSBundle mainBundle] pathForResource:@"HTML" ofType:@"txt"];
     
     NSData *data = [NSData dataWithContentsOfFile:html];
-    
+
     TFHpple *document = [[TFHpple alloc] initWithHTMLData:data];
     
-    NSString *tutorialsXpathQueryString = @"//div[@id='quickLookup']/table/tbody/tr"; ///td
+    NSString *tutorialsXpathQueryString = @"//div[@id='content-main']/div/table/tbody/tr"; ///td
     NSArray *tutorialsNodes = [document searchWithXPathQuery:tutorialsXpathQueryString];
     
+    NSArray *classInformation = ((TFHppleElement*)[tutorialsNodes objectAtIndex:0]).children;
+    NSString *className = ((TFHppleElement*)classInformation[1]).text;
+    NSString *grade = ((TFHppleElement*)classInformation[7]).text;
     
-    for (int i = 3; i < tutorialsNodes.count - 9; i++) {
-        
+    for (int i = 5; i < tutorialsNodes.count - 1; i++) {
         TFHppleElement *entireClass = (TFHppleElement*)[tutorialsNodes objectAtIndex:i];
-        NSArray *classDetails = entireClass.children;
-
-        NSString *className = ((TFHppleElement*)classDetails[14]).content;
+        NSArray *assignmentDetails = [entireClass children];
         
-        CiassInformation *q1Information = [self getClassInformation:((TFHppleElement*)classDetails[16])];
-        CiassInformation *q2Information = [self getClassInformation:((TFHppleElement*)classDetails[18])];
-        CiassInformation *x1Information = [self getClassInformation:((TFHppleElement*)classDetails[20])];
-        CiassInformation *q3Information = [self getClassInformation:((TFHppleElement*)classDetails[22])];
-        CiassInformation *q4Information = [self getClassInformation:((TFHppleElement*)classDetails[24])];
-        CiassInformation *x2Information = [self getClassInformation:((TFHppleElement*)classDetails[26])];
-        
-        StudentCIass *class = [[StudentCIass alloc] initWithAllInformation:className q1:q1Information q2:q2Information x1:x1Information q3:q3Information q4:q4Information x2:x2Information];
-        
-        [self.studentClasses addObject:class];
-    }
-    
-    for(StudentCIass *class in self.studentClasses) {
-        NSLog(class.description);
+        for(int y = 0; y < assignmentDetails.count; y++) {
+            TFHppleElement *value = (TFHppleElement*)assignmentDetails[y];
+            
+            if(value.content.length > 0) {
+                NSLog(@"%i\t%i\t%@", i, y, value.content);
+            }
+        }
     }
 }
+
 
 - (CiassInformation*) getClassInformation:(TFHppleElement*)attributes
 {
-
     NSString *grade = attributes.content;
-    NSString *link = [self parseLinkFromTD:attributes.raw];
+    NSString *link = [NSString stringWithFormat:@"https://pschool.princetonk12.org/guardian/%@",
+                      [self parseLinkFromTD:attributes.raw]];
     
     return [[CiassInformation alloc] initWithInformation:grade link:link];
 }
-
 
 - (NSString*) parseLinkFromTD:(NSString*)string
 {
@@ -117,15 +111,66 @@
 {
     NSString *currentURL = webView.request.URL.absoluteString;
     
+    NSLog(@"CURRENT URL: %@", currentURL);
+    
     //If we are at the basic login page
     if([currentURL isEqualToString:@"https://pschool.princetonk12.org/public/home.html"]) {
                 [self injectInformation:[self.keychain[@"isStudent"] isEqualToString:@"YES"] username:self.keychain[@"username"] password:self.keychain[@"password"]];
     }
     
-    else {
+    else if([currentURL isEqualToString:@"https://pschool.princetonk12.org/guardian/home.html"]){
+        NSString *html = [self.webView stringByEvaluatingJavaScriptFromString:@"document.documentElement.outerHTML"];
+        
+        NSData *data = [html dataUsingEncoding:NSUTF8StringEncoding];
+        TFHpple *document = [[TFHpple alloc] initWithHTMLData:data];
+        
+        NSString *tutorialsXpathQueryString = @"//div[@id='quickLookup']/table/tbody/tr"; ///td
+        NSArray *tutorialsNodes = [document searchWithXPathQuery:tutorialsXpathQueryString];
+        
+        for (int i = 3; i < tutorialsNodes.count - 9; i++) {
+            
+            TFHppleElement *entireClass = (TFHppleElement*)[tutorialsNodes objectAtIndex:i];
+            NSArray *classDetails = entireClass.children;
+            
+            NSString *className = ((TFHppleElement*)classDetails[14]).content;
+            
+            CiassInformation *q1Information = [self getClassInformation:((TFHppleElement*)classDetails[16])];
+            CiassInformation *q2Information = [self getClassInformation:((TFHppleElement*)classDetails[18])];
+            CiassInformation *x1Information = [self getClassInformation:((TFHppleElement*)classDetails[20])];
+            CiassInformation *q3Information = [self getClassInformation:((TFHppleElement*)classDetails[22])];
+            CiassInformation *q4Information = [self getClassInformation:((TFHppleElement*)classDetails[24])];
+            CiassInformation *x2Information = [self getClassInformation:((TFHppleElement*)classDetails[26])];
+            
+            StudentCIass *class = [[StudentCIass alloc] initWithAllInformation:className q1:q1Information q2:q2Information x1:x1Information q3:q3Information q4:q4Information x2:x2Information];
+            
+            [self.studentClasses addObject:class];
+        }
+        
+        for(StudentCIass *class in self.studentClasses) {
+            NSLog(class.description);
+        }
+        
+        NSString *tempLink = ((StudentCIass*)self.studentClasses[0]).q1.link;
+        [self.webView loadRequest:[NSURLRequest requestWithURL:[NSURL URLWithString:tempLink]]];
+    }
     
+    //Specific class grades
+    else if([currentURL containsString:@"https://pschool.princetonk12.org/guardian/scores.html?frn="]) {
+        NSString *html = [self.webView stringByEvaluatingJavaScriptFromString:@"document.documentElement.outerHTML"];
         
+        NSData *data = [html dataUsingEncoding:NSUTF8StringEncoding];
+        TFHpple *document = [[TFHpple alloc] initWithHTMLData:data];
         
+        NSString *tutorialsXpathQueryString = @"//div[@id='quickLookup']/table/tbody/tr"; ///td
+        NSArray *tutorialsNodes = [document searchWithXPathQuery:tutorialsXpathQueryString];
+        
+        for (int i = 3; i < tutorialsNodes.count - 9; i++) {
+            
+            TFHppleElement *entireClass = (TFHppleElement*)[tutorialsNodes objectAtIndex:i];
+            NSArray *classDetails = entireClass.children;
+            
+            NSString *className = ((TFHppleElement*)classDetails[14]).content;
+        }
     }
 
 }
@@ -152,21 +197,6 @@
 {
     NSLog(@"Started loading");
 }
-
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
-}
-
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
 
 - (IBAction)editSettings:(id)sender {
     self.settingsViewController = [[SettingsViewController alloc] initWithNibName:@"SettingsViewController" bundle:nil webView:self.webView];
